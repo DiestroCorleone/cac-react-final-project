33a2c13edbde8a82cba139fd6a785234d2232429
DiestroCorp <matias.perez.mcd@gmail.com>
2022-12-15 20:00:54 -0300

    Removido import innecesario de NotFound.

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import profilePicURL from "../images/coder-logo.svg"
 
 export default function NotFound() {
-  return ( 
+  return (
     <div className="width-responsive pad-mid">
       <h1>Error 404!</h1>
       <p>La página que buscás no existe o no está disponible.</p>
       <br />
       <Link to="/">
-        <button className="pad-mid back-black color-grey">Volver a inicio</button>
+        <button className="pad-mid back-black color-grey">
+          Volver a inicio
+        </button>
       </Link>
     </div>
   );
